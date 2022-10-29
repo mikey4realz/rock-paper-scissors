a1f77d6399c4fb67f6680d23fb9beb84692351ca
Michael Strade <35234473+mikey4realz@users.noreply.github.com>
2022-10-29 19:37:48 +0000

    rock paper scissors!

--- main.py
+++ main.py
@@ -28,16 +28,22 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
-player_choice = int(input("ROCK, PAPER, SCISSORS!!! Type 0 for Rock, 1, for Paper, 2 for Scissors:\n"))
+player_choice = int(input("ROCK, PAPER, SCISSORS, SHOW!!! Type 0 for Rock, 1, for Paper, 2 for Scissors:\n"))
 computer_choice = random.randint(0,2)
 choices = [rock,paper,scissors]
 
-print(f"You: {choices[player_choice]}\nvs\n\nComputer: \n{choices[computer_choice]}")
+if(player_choice != 0 and player_choice != 1 and player_choice != 2):
+  print("ERROR! You have chosen an invalid choice. A choice be will chosen for you at random.\n\n")
+  player_choice = random.randint(0,2)
+
+print(f"\nYou: {choices[player_choice]}\nvs\n\nComputer: \n{choices[computer_choice]}")
 
 if player_choice == 0 and computer_choice == 2:
   print("You Win!")
 elif player_choice > computer_choice:
   print("You Win!")
+elif player_choice == computer_choice:
+  print("It is a Draw.")
 else:
   print("You Lose.")
 
